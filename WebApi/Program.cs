ef3e43237f0d66002eb75be035f0f2fe66e566bf
lourencofelipe <felipe.pucsc@gmail.com>
2023-03-31 15:13:50 +1300

    feat: Adding architectur

---
+++ WebApi/Program.cs
@@ -0,0 +1,34 @@
+using Application.DI;
+using Infrastructure.DI;
+using Presentation.DI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services
+	.AddApplication()
+	.AddInfrastructure()
+	.AddPresentation();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
